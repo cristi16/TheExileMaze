f0b59c501de45f2a8be1b89d3ac12a9a998743d3
cristi16 <patrascu.cristinel@gmail.com>
2014-10-31 14:29:44 +0100

    first commit

---
+++ Assets/FitToScreen.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FitToScreen : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Projectile.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Projectile : MonoBehaviour
+{
+    public float speed = 3f;
+    Vector3 direction;
+
+    IEnumerator Move()
+    {
+        while(true)
+        {
+            rigidbody.MovePosition(transform.position + direction * Time.deltaTime * speed);
+            yield return null;
+        }
+    }
+
+    public void StartMovement(Vector3 forward)
+    {
+        direction = forward;
+        StartCoroutine(Move());
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        var hit = col.gameObject.GetComponent<EnvironmentObject>();
+        if(hit != null)
+        {
+            hit.PlayProjectileHitSound(col.contacts[0].point);
+        }
+    }
+}
